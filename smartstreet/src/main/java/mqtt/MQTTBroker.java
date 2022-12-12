0cb2a4320ec46dcbf905875621d4533102287c35
DavidRuizg <david.ruizg@edu.uah.es>
2022-12-12 15:37:04 +0100

    asd

--- smartstreet/src/main/java/mqtt/MQTTBroker.java
+++ smartstreet/src/main/java/mqtt/MQTTBroker.java
@@ -4,7 +4,7 @@ public class MQTTBroker
 {		
 	private static int qos = 2;
 	//Aqui tenemos que poner la IP de nuestro broker
-	private static String broker = "tcp://127.0.0.1:1883";
+	private static String broker = "tcp://172.20.10.10:1883";
 	private static String clientId = "smartStreet";
 		
 	public MQTTBroker()
